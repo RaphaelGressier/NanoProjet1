c0a8048d5b453128fd073914f5cb71328e2132ef
Raphaël Gressier <gressier.raphael@me.Com>
2012-11-26 15:18:33 +0100

    AJout de fichiers textes DEMO

--- Raphael_GRESSIER/bin/idem.sh
+++ Raphael_GRESSIER/bin/idem.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #  idem.sh
-#  
+#  idem.sh écrit sur stdout tous les noms complets
+#  (répertoires et nom de base du fichier) des fichiers correspondant à expression
+#  dans les arborescences de répertoires de chaque chemin mentionné.
 #
 #  Created by Raphaël Gressier on 26/11/12.
-#
+
+
